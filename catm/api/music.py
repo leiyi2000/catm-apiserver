4282f44d493ea4a385e152f0a7ef5dda3db5f549
ylei <2457738122@qq.com>
2024-03-27 20:24:24 +0800

    上传和获取音乐资源

--- catm/api/music.py
+++ catm/api/music.py
@@ -1,18 +1,54 @@
-from typing import List, Literal
+from typing import Generator, List
 
 import os
 from uuid import UUID
 
+from fastapi.responses import StreamingResponse
 from fastapi import APIRouter, Depends, Body, Path, UploadFile, File
 
 from catm import models
 from catm.settings import FILE_STORAGE
-from catm.constants import MusicStatus
 from catm.response import ErrorResponse
 from catm.auth import JwtAuth, Credential
+from catm.constants import MusicStatus, MusicResourcesType
 
 
 router = APIRouter()
+
+
+def resources_store_path(
+    music_id: str | UUID,
+    type: MusicResourcesType,
+) -> str:
+    """获取音乐资源存储路径.
+
+    Args:
+        music_id (str | UUID): 音乐ID.
+        type (MusicResourcesType): 存储文件类型.
+
+    Returns:
+        str: 资源存储路径.
+    """
+    music_id = str(music_id)
+    dir = os.path.join(FILE_STORAGE, "music", type)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir + "/" + music_id
+
+
+def stream_resources(file_path: str, size: int = 4096) -> Generator[bytes, None, None]:
+    """流式读取音乐资源.
+
+    Args:
+        file_path (str): 文件路径.
+        size (int): 每次读取大小.
+
+    Yields:
+        Generator[bytes]: 文件流.
+    """
+    with open(file_path, 'rb') as f:
+        while chunk := f.read(size):
+            yield chunk
 
 
 @router.post(
@@ -36,15 +72,15 @@ async def create(
 
 
 @router.get(
-    "/{id}",
-    description="获取音乐信息-(200 未查询到音乐)",
+    "/read/{id}",
+    description="获取音乐信息-(300 未查询到音乐)",
 )
 async def read(
     id: UUID = Path(),
 ):
     music = await models.Music.get(id=id)
     if music is None:
-        return ErrorResponse(code=200, msg="not found music")
+        return ErrorResponse(code=300, msg="not found music")
     return music
 
 
@@ -62,8 +98,8 @@ async def reads(
 
 
 @router.put(
-    "/{id}",
-    description="更新音乐信息-(200 未查询到音乐)",
+    "/update/{id}",
+    description="更新音乐信息-(300 未查询到音乐)",
 )
 async def update(
     credential: Credential = Depends(JwtAuth),
@@ -74,7 +110,7 @@ async def update(
 ):
     music = await models.Music.get_or_none(id=id, creator=credential.user_id)
     if music is None:
-        return ErrorResponse(code=200, msg="not found music")
+        return ErrorResponse(code=300, msg="not found music")
     music.name = name
     music.play_url = play_url
     music.singer = singer
@@ -82,34 +118,9 @@ async def update(
     return music
 
 
-def music_store_path(
-    music_id: str | UUID,
-    type: Literal["audio", "cover", "lyric"],
-    suffix: Literal["m4a", ""] = "",
-) -> str:
-    """获取音乐资源存储路径.
-
-    Args:
-        music_id (str | UUID): 音乐ID.
-        type (Literal["audio", "cover", "lyric"]): 存储文件类型.
-        suffix (Literal["m4a", ""]): 文件后缀名.
-
-    Returns:
-        str: 资源存储路径.
-    """
-    music_id = str(music_id)
-    dir = os.path.join(FILE_STORAGE, "music", type)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    if suffix:
-        return dir + "/" + music_id + "." + suffix
-    else:
-        return dir + "/" + music_id
-
-
 @router.post(
     "/upload/audio/{id}",
-    description="上传音乐",
+    description="上传音乐-(300 未查询到音乐)",
 )
 async def upload_audio(
     credential: Credential = Depends(JwtAuth),
@@ -117,11 +128,8 @@ async def upload_audio(
     audio: UploadFile = File(...),
 ):
     if not await models.Music.filter(id=id, creator=credential.user_id).exists():
-        return ErrorResponse(code=200, msg="not found music")
-    # 获取文件后缀
-    suffix = audio.filename.split(".")[-1]
-    assert suffix == "m4a", "only support m4a"
-    file_path = music_store_path(id, "audio", suffix)
+        return ErrorResponse(code=300, msg="not found music")
+    file_path = resources_store_path(id, MusicResourcesType.audio)
     # 流式上传
     with open(file_path, "wb") as file:
         while chunk := await audio.read(1024):
@@ -129,3 +137,62 @@ async def upload_audio(
     # TODO ylei 验证m4a文件完整性
     await models.Music.filter(id=id).update(status=MusicStatus.ready)
     return "ok"
+
+
+@router.post(
+    "/upload/cover/{id}",
+    description="上传音乐封面-(300 未查询到音乐)",
+)
+async def upload_cover(
+    credential: Credential = Depends(JwtAuth),
+    id: UUID = Path(),
+    cover: str = Body(),
+):
+    if not await models.Music.filter(id=id, creator=credential.user_id).exists():
+        return ErrorResponse(code=300, msg="not found music")
+    file_path = resources_store_path(id, MusicResourcesType.cover)
+    # 流式上传
+    with open(file_path, "w") as file:
+        file.write(cover)
+    return "ok"
+
+
+@router.post(
+    "/upload/lyric/{id}",
+    description="上传音乐歌词-(300 未查询到音乐)",
+)
+async def upload_lyric(
+    credential: Credential = Depends(JwtAuth),
+    id: UUID = Path(),
+    lyric: str = Body(),
+):
+    if not await models.Music.filter(id=id, creator=credential.user_id).exists():
+        return ErrorResponse(code=300, msg="not found music")
+    file_path = resources_store_path(id, MusicResourcesType.lyric)
+    # 流式上传
+    with open(file_path, "w") as file:
+        file.write(lyric)
+    return "ok"
+
+
+@router.get(
+    "/resources/{type}/{id}",
+    description="获取音乐资源-(300 未查询到音乐)",
+)
+async def get_audio(
+    id: UUID = Path(),
+    type: MusicResourcesType = Path(),
+):
+    file_path = resources_store_path(id, type)
+    if not await models.Music.filter(id=id).exists() or not os.path.exists(file_path):
+        return ErrorResponse(code=300, msg="not found music")
+    total_length = os.path.getsize(file_path)
+    if type == MusicResourcesType.audio:
+        media_type = 'audio/m4a'
+    else:
+        media_type = 'text/plain'
+    return StreamingResponse(
+        stream_resources(file_path), 
+        media_type=media_type,
+        headers={'Content-Length': str(total_length)}
+    )
--- catm/constants.py
+++ catm/constants.py
@@ -1,4 +1,7 @@
 """常量"""
+from enum import StrEnum
+
+
 class MusicStatus:
     """音乐状态"""
 
@@ -8,3 +11,14 @@ class MusicStatus:
     ready = "ready"
     # 损坏
     broken = "broken"
+
+
+class MusicResourcesType(StrEnum):
+    """音乐类型"""
+
+    # 音频
+    audio = "audio"
+    # 封面
+    cover = "cover"
+    # 歌词
+    lyric = "lyric"
